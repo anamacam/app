6ac347f99cd3e99bbe090fd352c84d13dcedfd1a
anamacam <anamacam.am@gmail.com>
2024-04-28 14:07:09 -0500

    se crea el formulario para registro de usuarios

--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,5 +1,50 @@
 <?php include("../../templates/header.php"); ?>
 
-Listar usuarios
+<?php include("../../templates/header.php"); ?>
+
+<br/>
+
+<div class="card">
+    <div class="card-header"> <a name="" id="" class="btn btn-primary"href="crear.php"role="button">
+            Agregar usuario
+            </a>
+        </div>
+    <div class="card-body">
+   
+
+
+<div class="table-responsive"-sm>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre del usuario</th>
+                <th scope="col">Contraseña</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            <tr class="">
+                <td scope="row">1</td>
+                <td>Ana M Castaño</td>
+                <td>*****</td>
+                <td>anama@mail.com</td>
+                <td>
+                    <input name="btneliminar"id="btneliminar"class="btn btn-info" type="button"value="Editar"/>
+                    <input name="btneliminar"id="btneliminar"class="btn btn-danger" type="button"value="Eliminar"/>
+                </td>
+                </td>
+            </tr>
+           
+        </tbody>
+    </table>
+    </div>
+    </div>
+</div>
+
+
+<?php include("../../templates/footer.php"); ?>
 
 <?php include("../../templates/footer.php"); ?>
